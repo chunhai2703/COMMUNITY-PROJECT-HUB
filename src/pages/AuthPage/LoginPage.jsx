f65199ac1d299eeb5fea9021a63d6079691f5b19
chunhai2703 <oceanvo437@gmail.com>
2025-02-05 11:14:47 +0700

    [EDIT]_LoginPage

--- src/pages/AuthPage/LoginPage.jsx
+++ src/pages/AuthPage/LoginPage.jsx
@@ -1,11 +1,9 @@
-import SnowFall from "../../components/Effect/SnowFall";
 import { LoginForm } from "../../components/LoginForm/LoginForm";
 
 
 const LoginPage = () => {
     return (
         <>
-            <SnowFall />
             <LoginForm />
         </>
     );
