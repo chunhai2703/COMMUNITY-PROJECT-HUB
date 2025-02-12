166e5ee82ac60029481fbc20f0248d7b2c0bc439
NguyenPhucPPP <nguyenphucdt36@gmail.com>
2025-02-12 21:32:21 +0700

    [Add]_Integrate api class and lesson class

--- src/components/ClassDetail/ClassDetail.jsx
+++ src/components/ClassDetail/ClassDetail.jsx
@@ -5,8 +5,11 @@ import { useState } from "react";
 import LessonClass from "./LessonClass";
 import TraineeList from "./TraineeList";
 import EvaluateResult from "./EvaluateResult";
+import { useParams } from "react-router-dom";
 const ClassDetail = () => {
     const [activeTab, setActiveTab] = useState("lesson");
+    const { classId } = useParams();
+    const { projectId } = useParams();
 
     return (
         <div>
@@ -15,54 +18,56 @@ const ClassDetail = () => {
                 <p className={style.classcode}>SE1715</p>
                 <p className={style.projectTitle}>Cách sử dụng AI</p>
             </div>
-            <div className={style.infoContainer}>
-                <div className={style.infoHeader}>
-                    <p><CalendarToday /> Thông tin chung</p>
-                </div>
-                <div className={style.info}>
-                    <div className="flex mb-5">
-                        <p className="mr-10 font-bold w-1/3"><RecordVoiceOver /> Giảng viên</p>
-                        <p> HoaDNT</p>
+            <div style={{margin: "0 20px"}}>
+                <div className={style.infoContainer}>
+                    <div className={style.infoHeader}>
+                        <p><CalendarToday /> Thông tin chung</p>
                     </div>
-                    <div className="flex mb-5">
-                        <p className="mr-10 font-bold w-1/3"><StarBorderIcon /> Sinh viên hỗ trợ</p>
-                        <div>
-                            <p>PhucNM1</p>
-                            <p>PhucNM2</p>
-                            <p>PhucNM3</p>
-                            <p>PhucNM4</p>
+                    <div className={style.info}>
+                        <div className="flex mb-5">
+                            <p className="mr-10 font-bold w-1/3"><RecordVoiceOver /> Giảng viên</p>
+                            <p> HoaDNT</p>
+                        </div>
+                        <div className="flex mb-5">
+                            <p className="mr-10 font-bold w-1/3"><StarBorderIcon /> Sinh viên hỗ trợ</p>
+                            <div>
+                                <p>PhucNM1</p>
+                                <p>PhucNM2</p>
+                                <p>PhucNM3</p>
+                                <p>PhucNM4</p>
+                            </div>
+                        </div>
+                        <div className="flex mb-5">
+                            <p className="mr-10 font-bold w-1/3"><StarBorderIcon /> Học viên</p>
+                            <p>25</p>
                         </div>
                     </div>
-                    <div className="flex mb-5">
-                        <p className="mr-10 font-bold w-1/3"><StarBorderIcon /> Học viên</p>
-                        <p>25</p>
-                    </div>
                 </div>
-            </div>
-            <div className={style.tabContainer}>
-                <button
-                    className={`${style.tab} ${activeTab === "lesson" ? style.active : ""}`}
-                    onClick={() => setActiveTab("lesson")}
-                >Buổi học</button>
-                <button
-                    className={`${style.tab} ${activeTab === "traineeList" ? style.active : ""}`}
-                    onClick={() => setActiveTab("traineeList")}
-                >Danh sách học viên</button>
-                <button
-                    className={`${style.tab} ${activeTab === "evaluateResult" ? style.active : ""}`}
-                    onClick={() => setActiveTab("evaluateResult")}
-                >Đánh giá kết quả</button>
-            </div>
-            <div style={{ backgroundColor: "#474D57", padding: "10px 30px", color: "white"}}>
-                <p style={{ fontSize: 24}}>Cách sử dụng AI trong giảng dạy</p>
-                <p style={{ fontSize: 14}}>SE1715</p>
-            </div>
+                <div className={style.tabContainer}>
+                    <button
+                        className={`${style.tab} ${activeTab === "lesson" ? style.active : ""}`}
+                        onClick={() => setActiveTab("lesson")}
+                    >Buổi học</button>
+                    <button
+                        className={`${style.tab} ${activeTab === "traineeList" ? style.active : ""}`}
+                        onClick={() => setActiveTab("traineeList")}
+                    >Danh sách học viên</button>
+                    <button
+                        className={`${style.tab} ${activeTab === "evaluateResult" ? style.active : ""}`}
+                        onClick={() => setActiveTab("evaluateResult")}
+                    >Đánh giá kết quả</button>
+                </div>
+                <div style={{ backgroundColor: "#474D57", padding: "10px 30px", color: "white"}}>
+                    <p style={{ fontSize: 24 }}>Cách sử dụng AI trong giảng dạy</p>
+                    <p style={{ fontSize: 14 }}>SE1715</p>
+                </div>
 
 
-            <div className={style.componentTab}>
-                {activeTab === "lesson" && <LessonClass />}
-                {activeTab === "traineeList" && <TraineeList />}
-                {activeTab === "evaluateResult" && <EvaluateResult />}
+                <div className={style.componentTab}>
+                    {activeTab === "lesson" && <LessonClass projectId={projectId} classId={classId} />}
+                    {activeTab === "traineeList" && <TraineeList />}
+                    {activeTab === "evaluateResult" && <EvaluateResult />}
+                </div>
             </div>
         </div>
 
--- src/components/ClassDetail/LessonClass.jsx
+++ src/components/ClassDetail/LessonClass.jsx
@@ -4,62 +4,84 @@ import style from "./LessonClass.module.css";
 import { Spinner } from "../Spinner/Spinner";
 import { Button, TextField } from "@mui/material";
 import { Edit } from "@mui/icons-material";
+import { GetAllLessonClassOfClass, UpdateLessonClass } from "../../services/ClassApi";
+import { toast } from "react-toastify";
 
-const data = [
-    {
-        lessonContent: "Trí tuệ nhân tạo - Công cụ đắc lực cho nhà giáo hiện đại",
-        startTime: "09:00",
-        endTime: "11:00",
-        date: "2025-06-16",
-        room: "306",
-    },
-    {
-        lessonContent: "Cá nhân hóa quá trình học tập với AI",
-        startTime: "09:00",
-        endTime: "11:00",
-        date: "2025-06-17",
-        room: "306",
-    },
-    {
-        lessonContent: "Tự động hóa các tác vụ lặp đi lặp lại",
-        startTime: "09:00",
-        endTime: "13:00",
-        date: "2025-06-18",
-        room: "309",
-    }
-];
+const LessonClass = ({ projectId, classId }) => {
+    const { control, handleSubmit, setValue, reset, formState: { errors } } = useForm({
+        defaultValues: { lessons: [] }
+    });
 
-const LessonClass = () => {
-    const { control, handleSubmit, setValue, formState: { errors } } = useForm({
-            defaultValues: { lessons: data }
-        });
     const [isEditing, setIsEditing] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [lessonClassList, setLessonClassList] = useState([]);
+
+    const formatOnlyDate = (dateString) => {
+        const date = new Date(dateString);
+        return new Intl.DateTimeFormat('vi-VN', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric'
+        }).format(date);
+    };
+    
+    const formatOnlyTime = (dateString) => {
+        const date = new Date(dateString);
+        return date.toLocaleTimeString('vi-VN', {
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false, // 24-hour format
+        });
+    };
+
+    const fetchLessonClassList = async () => {
+        try {
+            const response = await GetAllLessonClassOfClass(classId);
+            const responseData = await response.json();
+            if (response.ok) {
+                setLessonClassList(responseData.result);
+                reset({ lessons: responseData.result });
+            } else {
+                console.error("Error fetching lesson classes");
+            }
+        } catch (error) {
+            console.error("API error:", error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
     useEffect(() => {
-        if (isEditing) {
-            data.forEach((lesson, index) => {
-                setValue(`lessons[${index}].startTime`, lesson.startTime);
-                setValue(`lessons[${index}].endTime`, lesson.endTime);
-                setValue(`lessons[${index}].date`, lesson.date);
-                setValue(`lessons[${index}].room`, lesson.room);
-            });
+        if (classId) {
+            fetchLessonClassList();
         }
-    }, [isEditing, setValue]);
+    }, [classId]);
 
     const toggleEdit = () => {
         setIsEditing(!isEditing);
     };
 
-    const onSubmit = (updatedData) => {
-        console.log("Updated Lessons:", updatedData.lessons);
-        setIsEditing(false);
+    const onSubmit = async (updatedData) => {
+        setIsLoading(true);
+        const formattedLessons = updatedData.lessons.map((lesson) => ({
+            lessonClassId: lesson.lessonClassId,
+            room: lesson.room,
+            startTime: new Date(`${lesson.date}T${lesson.startTime}`).toISOString(),
+            endTime: new Date(`${lesson.date}T${lesson.endTime}`).toISOString(),
+        }))
+
+        const response = await UpdateLessonClass(projectId, formattedLessons)
+        const responseData = await response.json();
+        if (response.ok) {
+            toast.success("Cập nhật thông tin buổi học thành công")
+            fetchLessonClassList();
+            setIsEditing(false);
+        } else {
+            toast.error(responseData.message)
+        }
+        setIsLoading(false);
     };
 
-    useEffect(() => {
-        setIsLoading(true);
-        setTimeout(() => setIsLoading(false), 500);
-    }, []);
 
     if (isLoading) {
         return <Spinner />;
@@ -85,87 +107,107 @@ const LessonClass = () => {
                     {isEditing ? "Lưu" : "Cập nhật"}
                 </Button>
             </div>
-            {data.map((lesson, index) => (
-                <div key={index} className={style.lessonClassItem}>
-                    <p className={style.lessonContent}>{lesson.lessonContent}</p>
-                    {isEditing ? (
-                        <div className={style.lessonEditForm}>
-                            <Controller
-                                name={`lessons[${index}].startTime`}
-                                control={control}
-                                rules={{ required: "Bắt buộc nhập thời gian bắt đầu" }}
-                                render={({ field }) => (
-                                    <TextField
-                                        className={style.fieldInput}
-                                        {...field}
-                                        type="time"
-                                        label="Thời gian bắt đầu"
-                                        variant="outlined"
-                                        error={!!errors?.lessons?.[index]?.startTime}
-                                        helperText={errors?.lessons?.[index]?.startTime?.message}
-                                    />
-                                )}
-                            />
 
-                            <Controller
-                                name={`lessons[${index}].endTime`}
-                                control={control}
-                                rules={{ required: "Bắt buộc nhập thời gian kết thúc" }}
-                                render={({ field }) => (
-                                    <TextField
-                                        className={style.fieldInput}
-                                        {...field}
-                                        type="time"
-                                        label="Thời gian kết thúc"
-                                        variant="outlined"
-                                        error={!!errors?.lessons?.[index]?.endTime}
-                                        helperText={errors?.lessons?.[index]?.endTime?.message}
-                                    />
-                                )}
-                            />
+            {lessonClassList.length === 0 ? (
+                <p>Không có dữ liệu lớp học.</p>
+            ) : (
+                lessonClassList.map((lesson, index) => (
+                    <div key={index} className={style.lessonClassItem}>
+                        <p className={style.lessonContent}>{lesson.lessonContent}</p>
+                        {isEditing ? (
+                            <div className={style.lessonEditForm}>
+                                <Controller
+                                    name={`lessons[${index}].startTime`}
+                                    control={control}
+                                    rules={{ required: "Bắt buộc nhập thời gian bắt đầu" }}
+                                    render={({ field }) => (
+                                        <TextField
+                                            className={style.fieldInput}
+                                            {...field}
+                                            type="time"
+                                            label="Thời gian bắt đầu"
+                                            slotProps={{
+                                                inputLabel: { shrink: true },
+                                            }}
+                                            variant="outlined"
+                                            error={!!errors?.lessons?.[index]?.startTime}
+                                            helperText={errors?.lessons?.[index]?.startTime?.message}
+                                        />
+                                    )}
+                                />
 
-                            <Controller
-                                name={`lessons[${index}].date`}
-                                control={control}
-                                rules={{ required: "Bắt buộc nhập ngày học" }}
-                                render={({ field }) => (
-                                    <TextField
-                                        className={style.fieldInput}
-                                        {...field}
-                                        type="date"
-                                        label="Ngày học"
-                                        variant="outlined"
-                                        error={!!errors?.lessons?.[index]?.date}
-                                        helperText={errors?.lessons?.[index]?.date?.message}
-                                        inputProps={{ min: new Date().toISOString().split("T")[0] }}
-                                    />
-                                )}
-                            />
+                                <Controller
+                                    name={`lessons[${index}].endTime`}
+                                    control={control}
+                                    rules={{ required: "Bắt buộc nhập thời gian kết thúc" }}
+                                    render={({ field }) => (
+                                        <TextField
+                                            className={style.fieldInput}
+                                            {...field}
+                                            type="time"
+                                            label="Thời gian kết thúc"
+                                            slotProps={{
+                                                inputLabel: { shrink: true },
+                                            }}
+                                            variant="outlined"
+                                            error={!!errors?.lessons?.[index]?.endTime}
+                                            helperText={errors?.lessons?.[index]?.endTime?.message}
+                                        />
+                                    )}
+                                />
 
-                            <Controller
-                                name={`lessons[${index}].room`}
-                                control={control}
-                                rules={{ required: "Bắt buộc nhập phòng học" }}
-                                render={({ field }) => (
-                                    <TextField
-                                        className={style.fieldInput}
-                                        {...field}
-                                        label="Phòng học"
-                                        variant="outlined"
-                                        error={!!errors?.lessons?.[index]?.room}
-                                        helperText={errors?.lessons?.[index]?.room?.message}
-                                    />
-                                )}
-                            />
+                                <Controller
+                                    name={`lessons[${index}].date`}
+                                    control={control}
+                                    rules={{ required: "Bắt buộc nhập ngày học" }}
+                                    render={({ field }) => (
+                                        <TextField
+                                            className={style.fieldInput}
+                                            {...field}
+                                            type="date"
+                                            label="Ngày học"
+                                            slotProps={{
+                                                inputLabel: { shrink: true },
+                                            }}
+                                            variant="outlined"
+                                            error={!!errors?.lessons?.[index]?.date}
+                                            helperText={errors?.lessons?.[index]?.date?.message}
+                                            inputProps={{ min: new Date().toISOString().split("T")[0] }}
+                                        />
+                                    )}
+                                />
 
-                        </div>
-                    ) : (
-                        <p className={style.lessonInfo}>
-                            {lesson.startTime} - {lesson.endTime} | {lesson.date} | {lesson.room}
-                        </p>
-                    )}
-                </div>
-            ))}
+                                <Controller
+                                    name={`lessons[${index}].room`}
+                                    control={control}
+                                    rules={{ required: "Bắt buộc nhập phòng học" }}
+                                    render={({ field }) => (
+                                        <TextField
+                                            className={style.fieldInput}
+                                            {...field}
+                                            label="Phòng học"
+                                            slotProps={{
+                                                inputLabel: { shrink: true },
+                                            }}
+                                            variant="outlined"
+                                            error={!!errors?.lessons?.[index]?.room}
+                                            helperText={errors?.lessons?.[index]?.room?.message}
+                                        />
+                                    )}
+                                />
+                            </div>
+                        ) : (
+                            <div>
+                                {(lesson.startTime || lesson.endTime || lesson.date || lesson.room) ? (
+                                    <p className={style.lessonInfo}>
+                                        {formatOnlyTime(lesson.startTime)} - {formatOnlyTime(lesson.endTime)} | {formatOnlyDate(lesson.startTime)} | Room: {lesson.room}
+                                    </p>
+                                ) : "Chưa có thông tin"}
+                            </div>
+                        )}
+                    </div>
+                ))
+            )}
         </form>
     );
 };
